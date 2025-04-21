51b0450be27bd749d66298ab19b1e0f6b4d6220f
raafa12 <rafabasanta@hotmail.com>
2025-04-21 13:21:33 +0200

    Preparación para despliegue en Netlify6.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'; // ✅
+
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: 'server',
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
 });
